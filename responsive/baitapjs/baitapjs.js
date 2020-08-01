c5ee9372c0caa38c084e41ace363eb2430d3397f
aragon <hungnguyen>
2020-08-01 21:39:35 +0700

    first commit

---
+++ responsive/baitapjs/baitapjs.js
@@ -0,0 +1,7 @@
+console.log(" Hello Mai Anh ");
+
+x = document.querySelector('n');
+x = addEventListener('click', function(){
+    console.log("Ban vua click vao toi");
+    document.querySelector('.anh').classList.toggle('sangphai');
+})
